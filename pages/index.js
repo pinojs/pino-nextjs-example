94ec9cfc5bfd25507969baff6a20957ff41ac224
Gal Dubitski <galmail@gmail.com>
2020-05-25 11:12:47 +0100

    wrapping if with brackets

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import logger from '../next-pino/logger'
 
 export default function Home(req) {
-  if (typeof window === 'undefined' && req && req.log)
+  if (typeof window === 'undefined' && req && req.log) {
     logger = req.log
+  }
 
   logger.info('Home page loaded')
 
